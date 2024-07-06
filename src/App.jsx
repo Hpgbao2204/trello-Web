f7cdd05108dfced867bbe893b34e59bc0447f7e7
hpgbao <hpgbao@hpgbao.hpgbao>
2024-07-06 14:03:12 +0700

    Config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Divider } from '@mui/material'
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
+import Typography from '@mui/material/Typography'
+import { pink } from '@mui/material/colors'
 
 function App() {
 
   return (
     <>
       <div>Hello world</div>
+      <Typography variant="h1" color="text.secondary">Hello world</Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
@@ -16,7 +18,6 @@ function App() {
       <br/>
       <AccessAlarmIcon/>
       <ThreeDRotation/>
-      
       <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
